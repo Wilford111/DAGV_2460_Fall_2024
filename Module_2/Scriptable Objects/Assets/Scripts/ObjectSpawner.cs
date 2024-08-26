ac58ef8bcf6a2603cf8628d3932b6afbe326a170
Wilford111 <11053051@uvu.edu>
2024-08-26 00:08:59 -0600

    Attempted to do something with Vector3 dataobj

--- Module_2/Scriptable Objects/Assets/Scripts/ObjectSpawner.cs
+++ Module_2/Scriptable Objects/Assets/Scripts/ObjectSpawner.cs
@@ -4,6 +4,7 @@ public class ObjectSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn;
     public Vector3Data spawnPosition;
+    //public GameObject textObj;
     
     void Start()
     {
@@ -12,12 +13,15 @@ public class ObjectSpawner : MonoBehaviour
 
     void SpawnObject()
     {
-        float randomX = Random.Range(-10f, 10f);
-        float randomY = Random.Range(-10f, 10f);
+        float randomX = Random.Range(-8f, 8f);
+        float randomY = Random.Range(-5f, 5f);
         Vector3 randomPosition = new Vector3(randomX, randomY, 0f);
-        
+
+        spawnPosition = ScriptableObject.CreateInstance<Vector3Data>();
         spawnPosition.position = randomPosition;
         
-        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+        Instantiate(objectToSpawn, spawnPosition.position, Quaternion.identity);
+        // GameObject spawnObj = Instantiate(objectToSpawn, spawnPosition.position, Quaternion.identity);
+        //spawnObj.textObj = textObj;
     }
 }
--- Module_2/Scriptable Objects/Assets/Scripts/TriggerEventBehavior.cs
+++ Module_2/Scriptable Objects/Assets/Scripts/TriggerEventBehavior.cs
@@ -9,5 +9,6 @@ public class TriggerEventBehavior : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+        Debug.Log("interaction");
     }
 }
