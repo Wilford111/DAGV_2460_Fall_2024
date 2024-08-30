7f62c1029c212374c8046f3bbe4f0c25af484352
Wilford111 <11053051@uvu.edu>
2024-08-30 14:33:03 -0600

    Completed Unit 2 Challenge
    Added respawn button, added particle effects on Player and HealthPowerDown

---
+++ Unit2-UnityEvents/Unity Events/Assets/Scripts/MouseDownBehavior.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseDownBehavior : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+}
