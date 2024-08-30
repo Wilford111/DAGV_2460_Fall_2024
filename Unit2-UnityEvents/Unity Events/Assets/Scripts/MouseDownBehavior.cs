fc8f437a4bb051cddcc9331177ad1109b45f03b7
Wilford111 <11053051@uvu.edu>
2024-08-30 14:38:45 -0600

    Added 2 more events in Unit 2 Challenge project

--- Unit2-UnityEvents/Unity Events/Assets/Scripts/MouseDownBehavior.cs
+++ Unit2-UnityEvents/Unity Events/Assets/Scripts/MouseDownBehavior.cs
@@ -5,8 +5,19 @@ public class MouseDownBehavior : MonoBehaviour
 {
     public UnityEvent mouseDownEvent;
 
+    public Renderer rend;
     private void OnMouseDown()
     {
         mouseDownEvent.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = Color.yellow;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = Color.white;
     }
 }
