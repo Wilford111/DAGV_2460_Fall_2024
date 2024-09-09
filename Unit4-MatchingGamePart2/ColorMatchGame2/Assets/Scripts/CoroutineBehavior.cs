528809ed3fc3e3c83acd5fe8ad61f6520af827e3
Wilford111 <11053051@uvu.edu>
2024-09-09 08:45:20 -0600

    Created new Unity project for Unit4

---
+++ Unit4-MatchingGamePart2/ColorMatchGame2/Assets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        Debug.Log("Start");
+        yield return new WaitForSeconds(3);
+        Debug.Log("Late Start");
+    }
+}
